eb4516a59598e72ebc22b9ea9b348c94a336f2d6
lawrence18365 <lawrencebrennan@gmail.com>
2026-03-19 19:47:25 -0700

    Add PostHog and tighten SEO page ownership

--- js/analytics.js
+++ js/analytics.js
@@ -3,18 +3,44 @@
     const metaIdValue = metaMeasurementId ? metaMeasurementId.content.trim() : '';
     const windowIdValue = typeof window.GA_MEASUREMENT_ID === 'string' ? window.GA_MEASUREMENT_ID.trim() : '';
     const GA_MEASUREMENT_ID = metaIdValue || windowIdValue;
-    const hasValidId = Boolean(GA_MEASUREMENT_ID);
+    const hasValidGaId = Boolean(GA_MEASUREMENT_ID);
+    const posthogApiKey = typeof window.POSTHOG_API_KEY === 'string' ? window.POSTHOG_API_KEY.trim() : '';
+    const posthogApiHost = typeof window.POSTHOG_API_HOST === 'string' && window.POSTHOG_API_HOST.trim()
+        ? window.POSTHOG_API_HOST.trim()
+        : 'https://us.i.posthog.com';
+    const posthogDefaults = typeof window.POSTHOG_DEFAULTS === 'string' && window.POSTHOG_DEFAULTS.trim()
+        ? window.POSTHOG_DEFAULTS.trim()
+        : '2026-01-30';
+    const hasValidPosthogConfig = Boolean(posthogApiKey);
+    const hasAnyAnalyticsProvider = hasValidGaId || hasValidPosthogConfig;
     const consentKey = 'cookieConsent';
     const queuedEvents = [];
 
-    window.trackEvent = function (name, params) {
-        if (!hasValidId) {
-            return;
-        }
+    function sendQueuedEvent(name, params) {
+        let sent = false;
+
         if (typeof window.gtag === 'function') {
             window.gtag('event', name, params || {});
+            sent = true;
+        }
+
+        if (window.posthog && typeof window.posthog.capture === 'function') {
+            window.posthog.capture(name, params || {});
+            sent = true;
+        }
+
+        return sent;
+    }
+
+    window.trackEvent = function (name, params) {
+        if (!hasAnyAnalyticsProvider) {
             return;
         }
+
+        if (sendQueuedEvent(name, params)) {
+            return;
+        }
+
         queuedEvents.push([name, params]);
     };
 
@@ -70,8 +96,18 @@
         });
     }
 
-    function loadAnalytics() {
-        if (!hasValidId || window.__gaLoaded) {
+    function flushQueuedEvents() {
+        const pendingEvents = queuedEvents.splice(0);
+
+        pendingEvents.forEach(([name, params]) => {
+            if (!sendQueuedEvent(name, params)) {
+                queuedEvents.push([name, params]);
+            }
+        });
+    }
+
+    function loadGaAnalytics() {
+        if (!hasValidGaId || window.__gaLoaded) {
             return;
         }
         window.__gaLoaded = true;
@@ -89,10 +125,94 @@
         script.async = true;
         script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
         document.head.appendChild(script);
+    }
 
-        queuedEvents.splice(0).forEach(([name, params]) => {
-            gtag('event', name, params || {});
+    function loadPosthogAnalytics() {
+        if (!hasValidPosthogConfig || window.__posthogLoaded) {
+            return;
+        }
+        window.__posthogLoaded = true;
+
+        (function (documentRef, posthogRef) {
+            let scriptEl;
+            let firstScript;
+            let methodIndex;
+            let apiMethods;
+            let targetRef;
+
+            if (posthogRef.__SV) {
+                return;
+            }
+
+            window.posthog = posthogRef;
+            posthogRef._i = posthogRef._i || [];
+            posthogRef.init = function (token, config, name) {
+                function createStub(obj, methodName) {
+                    const splitName = methodName.split('.');
+                    if (splitName.length === 2) {
+                        obj = obj[splitName[0]];
+                        methodName = splitName[1];
+                    }
+                    obj[methodName] = function () {
+                        obj.push([methodName].concat(Array.prototype.slice.call(arguments, 0)));
+                    };
+                }
+
+                scriptEl = documentRef.createElement('script');
+                scriptEl.type = 'text/javascript';
+                scriptEl.crossOrigin = 'anonymous';
+                scriptEl.async = true;
+                scriptEl.src = config.api_host.replace('.i.posthog.com', '-assets.i.posthog.com') + '/static/array.js';
+                firstScript = documentRef.getElementsByTagName('script')[0];
+                firstScript.parentNode.insertBefore(scriptEl, firstScript);
+
+                targetRef = posthogRef;
+                if (typeof name !== 'undefined') {
+                    targetRef = posthogRef[name] = [];
+                } else {
+                    name = 'posthog';
+                }
+
+                targetRef.people = targetRef.people || [];
+                targetRef.toString = function (includeStub) {
+                    let str = 'posthog';
+                    if (name !== 'posthog') {
+                        str += '.' + name;
+                    }
+                    if (!includeStub) {
+                        str += ' (stub)';
+                    }
+                    return str;
+                };
+                targetRef.people.toString = function () {
+                    return targetRef.toString(1) + '.people (stub)';
+                };
+
+                apiMethods = 'init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug'.split(' ');
+
+                for (methodIndex = 0; methodIndex < apiMethods.length; methodIndex += 1) {
+                    createStub(targetRef, apiMethods[methodIndex]);
+                }
+
+                posthogRef._i.push([token, config, name]);
+            };
+            posthogRef.__SV = 1;
+        }(document, window.posthog || []));
+
+        window.posthog.init(posthogApiKey, {
+            api_host: posthogApiHost,
+            defaults: posthogDefaults,
         });
+    }
+
+    function loadAnalytics() {
+        if (!hasAnyAnalyticsProvider) {
+            return;
+        }
+
+        loadGaAnalytics();
+        loadPosthogAnalytics();
+        flushQueuedEvents();
     }
 
     function ensureCookieBanner() {
--- js/site-config.js
+++ js/site-config.js
@@ -1,2 +1,11 @@
 // Set your GA4 measurement ID here, for example: G-XXXXXXXXXX
 window.GA_MEASUREMENT_ID = window.GA_MEASUREMENT_ID || '';
+
+// Set your PostHog project API key here.
+window.POSTHOG_API_KEY = window.POSTHOG_API_KEY || 'phc_8e0oSYWxRaJ2YdfdmS4ZRvpC88FlM8PhNxgBNeNXa9a';
+
+// Update this if your PostHog project is not hosted in the US cloud.
+window.POSTHOG_API_HOST = window.POSTHOG_API_HOST || 'https://us.i.posthog.com';
+
+// Keep this aligned with the current PostHog defaults snapshot from their docs.
+window.POSTHOG_DEFAULTS = window.POSTHOG_DEFAULTS || '2026-01-30';
